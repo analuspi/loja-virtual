5ec837b6612b8b21a88d9f4a1c6040c69d35b17a
Ana <analucia.spi02@gmail.com>
2020-05-09 22:39:35 -0400

    fix bug title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 
-function Title(name, title) {
+function Title(props) {
     return (
         <div className="row">
            <div className="col-10 mx-auto my-2 text-center text-title">
                 <h1 className="text-capitalize font-weight-bold">
-                    {name} <strong className="text-blue">{title}
+                    {props.name} <strong className="text-blue">{props.title}
                     </strong>
                 </h1>
             </div> 
